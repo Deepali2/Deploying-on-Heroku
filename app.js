71f63dd486348c7e5f6ed5d6e450b036d44719fe
Deepali2 <hdgarg@gmail.com>
2018-11-21 13:56:28 -0800

    change port to 3000

--- app.js
+++ app.js
@@ -13,6 +13,6 @@ app.get("/about", function(req, res) {
   res.render("about");
 });
 
-app.listen(3001, function() {
+app.listen(3000, function() {
   console.log("I am listening on port 3001");
 });
