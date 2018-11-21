09fa629c87cf938d37e0a3206ac86bfff274ac72
Deepali2 <hdgarg@gmail.com>
2018-11-21 14:03:34 -0800

    add process.env.PORT to app.listen

--- app.js
+++ app.js
@@ -13,6 +13,6 @@ app.get("/about", function(req, res) {
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("I am listening on port 3001");
+app.listen(process.env.PORT || 3000, function() {
+  console.log("I am listening on port 3000");
 });
